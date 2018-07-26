89ea017969068cf0c397fdd99ff3804ef8079a89
Lyndon White <oxinabox@ucc.asn.au>
2018-07-26 16:12:26 +0800

    remove debuggingline

--- scripts/jupyter/run_nbconvert.sh
+++ scripts/jupyter/run_nbconvert.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
-jupyter nbconvert  --to markdown "$1" --config jekyll_config.py --template=jekyll.tpl --Application.log_level='DEBUG' 
+jupyter nbconvert  --to markdown "$1" --config jekyll_config.py --template=jekyll.tpl --Application.log_level='DEBUG'
+
 
 outputname=$(basename "$1" .ipynb)
 outputpath=${1%.ipynb}
@@ -8,4 +9,4 @@ outputpath=${1%.ipynb}
 postname=$(date +"%Y-%m-%d-")$(echo $outputname | tr " " -)
 mv "$outputpath.md" "../../_posts/$postname.md"
 
-mv  "$outputname"_files"" ../../posts_assets/
+mv  "$outputname"_files ../../posts_assets/
